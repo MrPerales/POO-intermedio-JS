1107e1f4ec760a7d3ef448f64b87f7df47a3e964
Carlos Perales <carlosperales050@gmail.com>
2023-02-18 21:02:45 -0600

    se agrego archivo  reto1.js (con detalles)

---
+++ reto1.js
@@ -0,0 +1,41 @@
+// Dado un objeto como el siguiente:
+
+const carlos = {
+    name: "Carlos",
+    approvedCourses: ["Course 1","Course 2"],
+    characteristic: {
+      age: 18,
+      colorHair: 'Negro',
+      hobbies: {
+        music: ['rock', 'punk', 'ska'],
+        movies: ['drama', 'horror', 'comedy'],
+      },
+    },
+    addCourse(newCourse) {
+      console.log("This", this);
+      console.log("This.approvedCourses", this.approvedCourses);
+      this.approvedCourses.push(newCourse);
+    }
+};
+//   Tu reto es crear una función que aplique Object.freeze 
+//   a TODOS LOS OBJETOS anidados de forma recursiva para 
+//   así realmente lograr bloquear todo el objeto. A esto se 
+//   le conoce comunmente como deepFreeze.
+
+
+
+function deepFreeze(obj){
+    
+    if(typeof obj ==='object'){
+        Object.freeze(obj);
+    }else{
+        return obj;
+    }
+
+    for(key in obj){
+        
+        deepFreeze(obj[key]);
+      
+    }
+
+}
